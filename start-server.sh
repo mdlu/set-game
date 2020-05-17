79a272502385327af701881bb0aa09d241f022d0
Richard Liu <rtliu@mit.edu>
2020-05-17 13:17:54 -0400

    Adds bash script for basic use

---
+++ start-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Script for running set game server.
+# Normally, you should be able to execute this script with
+#
+#   ./start-server.sh
+#
+# and it should do The Right Thing(tm) by default. However, it does take some
+# arguments so that you can tweak it for your set gane setup.
+#
+#   1) how many properties to play with (defaults to 4)
+
+if [ $# -eq 1 ] && [ "$1" = "--help" ]; then
+	echo "Usage: $0 [ATTRIBUTES=100]"
+	exit 1
+fi
+
+# Default to 4 attributes unless otherwise specified
+attr=4
+if [ $# -gt 0 ]; then
+	attr="$1"
+fi
+
+java -ea -cp bin memory.ServerMain 8080 "$attr"
